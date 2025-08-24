0c57b9b2e5108236031373df948a1945159de815
Rodrigo <rlucchese@gmail.com>
2025-08-24 19:14:56 -0300

    Adicionado android e IOS

---
+++ Apponta/android/app/src/main/java/br/app/grape/MainActivity.java
@@ -0,0 +1,18 @@
+package br.app.grape;
+
+import android.os.Bundle;
+
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Initializes the Bridge
+
+  }
+}
